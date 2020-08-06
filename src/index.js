4b166c86583e401bc435b6e5b155e7990336ada5
Almighty Stranger <sufyankhan9678@gmail.com>
2020-08-06 12:28:09 +0530

    kmdkf

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter as Router} from "react-router-dom";
 import App from "./components/app";
-ReactDOM.render(<Router><App/></Router>,document.getElementById("root"));
+ReactDOM.render(<Router  basename={process.env.PUBLIC_URL}><App/></Router>,document.getElementById("root"));
