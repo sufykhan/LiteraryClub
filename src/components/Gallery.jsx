83d21db346825f65bc0412a8397738595d88bc40
Almighty Stranger <sufyankhan9678@gmail.com>
2020-08-22 02:33:30 +0530

    Done

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import images from "./ima";
-import ResponsiveGallery from "react-responsive-gallery";
+import Gallery from "react-grid-gallery";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import "./Gallery.css";
+import LazyLoad from 'react-lazyload';
+
+
 function Gal()
 {
+
     return(
    <div class="container">
      <div class="jumbotron">
        <h1><i class="fa fa-camera-retro"></i> The <a href="/" style={{textDecoration:"none",color:"#FFFFFF"}}>Literary Club</a> Gallery</h1>
        <p>A bunch of cool images I didn't take(I wish)</p>
       </div>
-      <ResponsiveGallery images={images} />
+      <LazyLoad>
+      <Gallery images={images} backdropClosesModal={true} />
+      </LazyLoad>
     </div>
 
 
--- src/components/ima.jsx
+++ src/components/ima.jsx
@@ -1,43 +1,107 @@
 const images = [
-    {
-      src:
-        "https://cdn.pixabay.com/photo/2017/01/14/12/59/iceland-1979445_960_720.jpg"
-    },
-    {
-      src:
-        "https://cdn.pixabay.com/photo/2019/06/12/15/07/cat-4269479_960_720.jpg"
-    },
-    {
-      src:
-        "https://cdn.pixabay.com/photo/2016/12/04/21/58/rabbit-1882699_960_720.jpg"
-    },
-    {
-      src:
-        "https://cdn.pixabay.com/photo/2014/07/08/12/36/bird-386725_960_720.jpg"
-    },
-    {
-      src:
-        "https://cdn.pixabay.com/photo/2015/10/12/15/46/fallow-deer-984573_960_720.jpg"
-    },
-    {
-      src:
-        "https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_960_720.jpg"
-    },
-    {
-      src:
-        "https://cdn.pixabay.com/photo/2013/09/22/15/29/prairie-dog-184974_960_720.jpg"
-    },
-    {
-      src:
-        "https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416_960_720.jpg"
-    },
-    {
-      src:
-        "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_960_720.jpg"
-    },
-    {
-      src:
-        "https://cdn.pixabay.com/photo/2019/03/09/17/30/horse-4044547_960_720.jpg"
-    }
+  {
+    src:"https://i.ibb.co/vj3smHC/Copy-of-DSC0100.jpg",
+    thumbnail:"https://i.ibb.co/vj3smHC/Copy-of-DSC0100.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174
+},        
+ {
+    src:"https://i.ibb.co/MGxMsyw/Copy-of-DSC0104.jpg"
+    ,
+    thumbnail:"https://i.ibb.co/MGxMsyw/Copy-of-DSC0104.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174 
+      
+  },
+{
+    src:"https://i.ibb.co/JdfpDGK/Copy-of-DSC-0786.jpg" 
+    ,
+    thumbnail:"https://i.ibb.co/JdfpDGK/Copy-of-DSC-0786.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src:"https://i.ibb.co/sV8GvG8/Copy-of-DSC1023.jpg"
+    ,
+    thumbnail:"https://i.ibb.co/sV8GvG8/Copy-of-DSC1023.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src:"https://i.ibb.co/M7w94q4/Copy-of-DSC0998.jpg" 
+    ,
+    thumbnail:"https://i.ibb.co/M7w94q4/Copy-of-DSC0998.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src:"https://i.ibb.co/7pJBhLb/Copy-of-DSC0992.jpg"
+    ,
+    thumbnail:"https://i.ibb.co/7pJBhLb/Copy-of-DSC0992.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src:"https://i.ibb.co/tDYg8cF/Screenshot-11.png" 
+    ,
+
+thumbnail:"https://i.ibb.co/tDYg8cF/Screenshot-11.png" ,    thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src:"https://i.ibb.co/9bDgFKK/Screenshot-12.png"
+    ,
+ 
+ thumbnail:"https://i.ibb.co/9bDgFKK/Screenshot-12.png"
+,   thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src:"https://i.ibb.co/Wgvn60Z/Screenshot-13.png"
+    ,
+ 
+ thumbnail:"https://i.ibb.co/Wgvn60Z/Screenshot-13.png"
+,   thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src:"https://i.ibb.co/WWB9V4W/Screenshot-14.png"
+    ,
+ 
+ thumbnail:"https://i.ibb.co/WWB9V4W/Screenshot-14.png"
+,   thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src:"https://i.ibb.co/rwGmXRF/Screenshot-15.png"
+    ,
+ 
+ thumbnail:"https://i.ibb.co/rwGmXRF/Screenshot-15.png"
+,   thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src:"https://i.ibb.co/zmD5hf3/Screenshot-17.png"
+    ,
+ 
+ thumbnail:"https://i.ibb.co/zmD5hf3/Screenshot-17.png"
+,   thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src: "https://i.ibb.co/sVnV5BF/Screenshot-18.png"
+    ,
+
+thumbnail: "https://i.ibb.co/sVnV5BF/Screenshot-18.png",    thumbnailWidth: 320,
+    thumbnailHeight: 174
+  },
+{
+    src:"https://i.ibb.co/CtNL8sJ/Screenshot-19.png"
+    ,
+ 
+ thumbnail:"https://i.ibb.co/CtNL8sJ/Screenshot-19.png"
+,   thumbnailWidth: 320,
+    thumbnailHeight: 174
+  }
   ];
   export default images;
